5dbf569734ca01800360b9d8dde318842308e62f
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-07 10:40:31 +1200

    completed step 6

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,6 +2,21 @@ import React, {useState} from 'react'
 
 const Table = ({ className, columns, rows, format, perPage }) => {
   const [page, setPage] = useState(0)
+  const [filterAirlines, setFilterAirlines] = useState('All Airlines')
+
+  const availableFilters = Array.from(
+    new Set(rows.map(r => format('airline', r['airline'])))
+  ).concat('All Airlines')
+
+  
+
+  rows = rows.filter(r => {
+    if (filterAirlines === 'All Airlines') return true
+    // get formatted airline value
+    const airlineName = format('airline', r['airline'])
+    // ---> passes filter if equal, otherwise not
+    return filterAirlines === airlineName
+  })
 
   // filter amount of rows based on page and perPage
   const paginatedRows = rows.slice(page, page + perPage)
@@ -24,8 +39,23 @@ const Table = ({ className, columns, rows, format, perPage }) => {
     )
   }
 
+  const handleAirlineFilter = (e) => {
+    setFilterAirlines(e.target.value)
+    setPage(0)
+  }
+
   return (
     <div>
+      <label htmlFor="airline-filter">Show routes on:</label>
+      <select 
+        id="airline-filter"
+        value={filterAirlines}
+        onChange={handleAirlineFilter}
+      >
+        {availableFilters.map(f => {
+          return <option key={f} value={f}>{f}</option>
+        })}
+      </select>
       <table className={className}>
         <thead>
           <tr>{columns.map(c => <th key={c.name}>{c.name}</th>)}</tr>
